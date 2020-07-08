cff10c11c779415a491ebb3d4bce27355fb8f31e
krafts <varun.net@gmail.com>
2020-07-08 11:41:58 -0400

    time: removing month from time format.

--- segment-time.go
+++ segment-time.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	pwl "github.com/justjanne/powerline-go/powerline"
 	"time"
+
+	pwl "github.com/justjanne/powerline-go/powerline"
 )
 
 func segmentTime(p *powerline) {
 	p.appendSegment("time", pwl.Segment{
-		Content:    time.Now().Format("01-02T15:04:05"),
+		Content:    time.Now().Format("02T15:04:05"),
 		Foreground: p.theme.TimeFg,
 		Background: p.theme.TimeBg,
 	})
