cf64179fcddcd22d384d527824ec8a9d40d0d28c
krafts <varun.net@gmail.com>
2021-03-11 22:25:11 -0500

    kube: more flexible way of managing envs.

--- segment-kube.go
+++ segment-kube.go
@@ -30,6 +30,59 @@ type KubeConfig struct {
 	CurrentContext string        `yaml:"current-context"`
 }
 
+type Environment struct {
+	Type       string `yaml:"type"`
+	Name       string `yaml:"name"`
+	Contains   []string `yaml:"contains"`
+	Prefix     []string `yaml:"prefix"`
+	Suffix     []string `yaml:"suffix"`
+}
+
+type Environments struct {
+	Environments []Environment `yaml:"environments"`
+	Remove       []string `yaml:"remove"`
+}
+
+func getEnvironments() Environments {
+	var k8sEnvs Environments
+	k8sEnvsFile := path.Join(homePath(), ".kubectl_context_ps1_environments.yaml")
+	stat, err := os.Stat(k8sEnvsFile)
+	if err == nil && !stat.IsDir() {
+		environments, err := ioutil.ReadFile(k8sEnvsFile)
+
+		// unmarshall it
+		err = yaml.Unmarshal(environments, &k8sEnvs)
+		if err == nil {
+		}
+	}
+	return k8sEnvs
+}
+
+func doesEnvMatch(cluster string, k8sEnvs *Environments, envType string) bool {
+	match := false
+
+	for i := 0; i < len(k8sEnvs.Environments); i++ {
+		envMatch := (envType == k8sEnvs.Environments[i].Type)
+
+		for j := 0; j < len(k8sEnvs.Environments[i].Contains); j++ {
+			if strings.Contains(cluster, k8sEnvs.Environments[i].Contains[j]) && envMatch {
+				return true
+			}
+		}
+		for j := 0; j < len(k8sEnvs.Environments[i].Prefix); j++ {
+			if strings.HasPrefix(cluster, k8sEnvs.Environments[i].Prefix[j]) && envMatch {
+				return true
+			}
+		}
+		for j := 0; j < len(k8sEnvs.Environments[i].Suffix); j++ {
+			if strings.HasSuffix(cluster, k8sEnvs.Environments[i].Suffix[j]) && envMatch {
+				return true
+			}
+		}
+	}
+	return match
+}
+
 func homePath() string {
 	env := "HOME"
 	if runtime.GOOS == "windows" {
@@ -70,7 +123,7 @@ func segmentKube(p *powerline) {
 
 	cluster := ""
 	//namespace := ""
-	environment := ""
+	//environment := ""
 	for _, context := range config.Contexts {
 		if context.Name == config.CurrentContext {
 			cluster = context.Name
@@ -79,19 +132,17 @@ func segmentKube(p *powerline) {
 		}
 	}
 
-	user, _ := os.LookupEnv("USER")
 	prod := false
 	staging := false
-	uat := false
+	qat := false
+	sandbox := false
 	dev := false
-	prodDomain := ""
-	stagingDomain := ""
-	uatDomain := ""
-	devDomain := ""
 	label_b := ""
 	label_e := ""
 	fg := p.theme.KubeClusterFg
 	bg := p.theme.KubeClusterBg
+
+	k8sEnvs := getEnvironments()
 
 	// When you use gke your clusters may look something like gke_projectname_availability-zone_cluster-01
 	// instead I want it to read as `cluster-01`
@@ -100,52 +151,7 @@ func segmentKube(p *powerline) {
 		segments := strings.Split(cluster, "_")
 		if len(segments) > 3 {
 			cluster = strings.Join(segments[3:], "_")
-			environment = segments[1]
-
-			k8sDomainsFile := path.Join(homePath(), ".kubectl_context_ps1_domains")
-			stat, err := os.Stat(k8sDomainsFile)
-			if err == nil && !stat.IsDir() {
-				domains, err := ioutil.ReadFile(k8sDomainsFile)
-				if err == nil {
-					for _, domain := range strings.Split(string(domains), "\n") {
-						if strings.HasPrefix(domain, "PROD=") {
-							prodDomain = strings.Split(domain, "=")[1]
-						} else if strings.HasPrefix(domain, "STAGING=") {
-							stagingDomain = strings.Split(domain, "=")[1]
-						} else if strings.HasPrefix(domain, "UAT=") {
-							uatDomain = strings.Split(domain, "=")[1]
-						} else if strings.HasPrefix(domain, "DEV=") {
-							devDomain = strings.Split(domain, "=")[1]
-						}
-					}
-				}
-			}
-
-			if environment == prodDomain {
-				prod = true
-				label_b = "PROD->>!!! "
-				label_e = " !!!<<-PROD"
-				fg = p.theme.KubeProdFg
-				bg = p.theme.KubeProdBg
-			} else if environment == stagingDomain {
-				staging = true
-				label_b = ""
-				label_e = " STAGING"
-				fg = p.theme.ShellVarFg
-				bg = p.theme.ShellVarBg
-			} else if environment == uatDomain {
-				staging = true
-				label_b = ""
-				label_e = " UAT"
-				fg = p.theme.GitNotStagedFg
-				bg = p.theme.GitNotStagedBg
-			} else if environment == devDomain && !strings.HasPrefix(cluster, user) {
-				dev = true
-				label_b = ""
-				label_e = " DEV"
-				fg = p.theme.KubeClusterFg
-				bg = p.theme.KubeClusterBg
-			}
+			//environment = segments[1]
 		}
 	}
 
@@ -158,11 +164,48 @@ func segmentKube(p *powerline) {
 		cluster = arnMatches[1]
 	}
 
+	if doesEnvMatch(cluster, &k8sEnvs, "PROD") {
+		prod = true
+		label_b = "PROD->>!!! "
+		label_e = " !!!<<-PROD"
+		fg = p.theme.KubeProdFg
+		bg = p.theme.KubeProdBg
+	} else if doesEnvMatch(cluster, &k8sEnvs, "Staging") {
+		staging = true
+		label_b = ""
+		label_e = " STAGING"
+		fg = p.theme.ShellVarFg
+		bg = p.theme.ShellVarBg
+	} else if doesEnvMatch(cluster, &k8sEnvs, "QAT") {
+		qat = true
+		label_b = ""
+		label_e = " QAT"
+		fg = p.theme.ShellVarFg
+		bg = p.theme.ShellVarBg
+		// fg = p.theme.GitNotStagedFg
+		// bg = p.theme.GitNotStagedBg
+	} else if doesEnvMatch(cluster, &k8sEnvs, "sandbox") {
+		sandbox = true
+		label_b = ""
+		label_e = " SANDBOX"
+		fg = p.theme.KubeClusterFg
+		bg = p.theme.KubeClusterBg
+	} else if doesEnvMatch(cluster, &k8sEnvs, "dev") {
+		dev = true
+		label_b = ""
+		label_e = " DEV"
+		fg = p.theme.KubeClusterFg
+		bg = p.theme.KubeClusterBg
+	}
+
 	// Only draw the icon once
 	//kubeIconHasBeenDrawnYet := false
 	if cluster != "" {
 		//kubeIconHasBeenDrawnYet = true
-		if prod || staging || uat || dev {
+		if prod || staging || qat || sandbox || dev {
+			for i := 0; i < len(k8sEnvs.Remove); i++ {
+				cluster = strings.Replace(cluster, k8sEnvs.Remove[i], "", -1)
+			}
 			p.appendSegment("kube-cluster", pwl.Segment{
 				Content:    fmt.Sprintf("%s%s%s", label_b, cluster, label_e),
 				Foreground: fg,
@@ -183,3 +226,47 @@ func segmentKube(p *powerline) {
 	// 	})
 	// }
 }
+
+
+
+/*
+sample of .kubectl_context_ps1_environments.yaml
+---
+environments:
+  - type: PROD
+    name: AWS PROD
+    contains:
+      - "111111111111"
+    prefix:
+      - np
+    suffix:
+  - type: Staging
+    name: AWS Staging
+    contains:
+      - "222222222222"
+    prefix:
+    suffix:
+  - type: sandbox
+    name: AWS Sandbox
+    contains:
+      - "333333333333"
+    prefix:
+    suffix:
+  - type: QAT
+    name: QAT
+    contains:
+    prefix:
+      - nq
+    suffix:
+  - type: dev
+    name: dev
+    contains:
+    prefix:
+      - nd
+    suffix:
+remove:
+  - .k8s.net.com.yaml
+  - arn:aws:eks:us-east-1:111111111111:cluster/
+  - arn:aws:eks:us-west-1:222222222222:cluster/
+  - arn:aws:eks:us-south-1:333333333333:cluster/
+*/
